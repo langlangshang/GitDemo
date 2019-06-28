99b56bdb840d1626c0ba3e7695b2874ee33e73ca
langlangshang <1206728627@qq.com>
2019-06-29 07:57:33 +0800

    第二次提交

--- src/main/java/com/lichuan/GitApplication.java
+++ src/main/java/com/lichuan/GitApplication.java
@@ -9,6 +9,7 @@ public class GitApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(GitApplication.class,args);
+        System.out.println("test");
         System.err.println("运行成功");
     }
 }
