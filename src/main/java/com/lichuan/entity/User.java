d7022bc4105cce1902c4ca96f23e75d556f2f89d
langlangshang <1206728627@qq.com>
2019-06-29 09:38:37 +0800

    第三次提交

---
+++ src/main/java/com/lichuan/entity/User.java
@@ -0,0 +1,61 @@
+package com.lichuan.entity;
+
+public class User {
+
+    private String uid;
+    private String username;
+    private String password;
+    private String age;
+    private String sex;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid='" + uid + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", age='" + age + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
